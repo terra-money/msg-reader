dd8502545eef4d5f9796df2d297bb957ff1813d4
James <james@terra.money>
2021-10-14 00:15:17 +0900

    fix typo

--- src/utility.ts
+++ src/utility.ts
@@ -1,4 +1,4 @@
 import { Coin, Coins } from "@terra-money/terra.js"
 
-export const formatCoins = (data: Coins.Data) => data.map(formatCoin).join(", ")
+export const formatCoins = (data: Coins.Data) => data.map(formatCoin).join(",")
 export const formatCoin = ({ amount, denom }: Coin.Data) => `${amount}${denom}`
